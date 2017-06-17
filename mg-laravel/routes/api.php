16de5a6957e2b39f9eb15a7753ed06c74159cd21
Luciano Reis <lucianoreis.dev@gmail.com>
2017-06-16 22:48:02 -0400

    [ADD] JWTAuth

--- mg-laravel/routes/api.php
+++ mg-laravel/routes/api.php
@@ -12,7 +12,11 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login','Auth\LoginController@authenticate');
+    Route::get('logout','Auth\LoginController@logout');
+    Route::get('check','Auth\LoginController@check');
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
