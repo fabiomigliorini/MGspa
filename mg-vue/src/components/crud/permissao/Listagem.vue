26a5dd7c3fbec922605004186ee965c364431c4c
Luciano Reis <lucianoreis.dev@gmail.com>
2017-07-06 08:11:08 -0400

    [UPD] Correção altura list-tile Permissões

--- mg-vue/src/components/crud/permissao/Listagem.vue
+++ mg-vue/src/components/crud/permissao/Listagem.vue
@@ -154,7 +154,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
   .list--two-line .list__tile {
     height: 50px;
   }
