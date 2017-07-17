bb70fecbb9139bf2dddf52eb07c5e4c42e9fefd5
Fabio Migliorini <fabio@mgpapelaria.com.br>
2017-07-17 15:21:00 -0400

    [UPD] MgLayout

---
+++ mg-quasar/src/components/layouts/MgLayout.vue
@@ -0,0 +1,121 @@
+<template>
+<q-layout>
+
+  <div slot="header" class="toolbar">
+    <button class="hide-on-drawer-visible" @click="$refs.drawer.toggle()">
+        <i>menu</i>
+      </button>
+    <q-toolbar-title :padding="1">
+      <slot name="title"></slot>
+    </q-toolbar-title>
+
+    <button @click="$refs.rightDrawer.open()">
+        <i>apps</i>
+      </button>
+  </div>
+
+  <q-tabs slot="navigation" v-if="navigation">
+    <slot name="navigation"></slot>
+  </q-tabs>
+
+  <q-drawer ref="drawer" v-if="drawer">
+    <slot name="drawer"></slot>
+  </q-drawer>
+
+  <!--
+  <router-view class="layout-view"></router-view>
+  -->
+
+  <div class="layout-view">
+    <slot name="content"></slot>
+  </div>
+
+  <q-drawer ref="rightDrawer" swipe-only right-side>
+
+    <div class="container">
+    <div class="list-header">
+      Right Side Drawer
+    </div>
+    </div>
+
+    <div class="row wrap">
+      <div class="auto text-center width-1of4" style="border: 1px solid blue">
+        <router-link :to="{ path: '/marca', params: {} }">
+          <i style="font-size: 48px;">account_circle</i>
+        </router-link>
+      </div>
+      <div class="auto text-center width-1of4" style="border: 1px solid blue">
+        <router-link :to="{ path: '/marca', params: {} }">
+          <i style="font-size: 48px;">account_circle</i>
+        </router-link>
+      </div>
+      <div class="auto text-center width-1of4" style="border: 1px solid blue">
+        <router-link :to="{ path: '/marca', params: {} }">
+          <i style="font-size: 48px;">account_circle</i>
+        </router-link>
+      </div>
+      <div class="auto text-center width-1of4" style="border: 1px solid blue">
+        <router-link :to="{ path: '/marca', params: {} }">
+          <i style="font-size: 48px;">account_circle</i>
+        </router-link>
+      </div>
+      <div class="auto text-center width-1of4" style="border: 1px solid blue">
+        <router-link :to="{ path: '/marca', params: {} }">
+          <i style="font-size: 48px;">account_circle</i>
+        </router-link>
+      </div>
+      <div class="auto text-center width-1of4" style="border: 1px solid blue">
+        <router-link :to="{ path: '/marca', params: {} }">
+          <i style="font-size: 48px;">account_circle</i>
+        </router-link>
+      </div>
+      <div class="auto text-center width-1of4" style="border: 1px solid blue">
+        <router-link :to="{ path: '/marca', params: {} }">
+          <i style="font-size: 48px;">account_circle</i>
+        </router-link>
+      </div>
+      <div class="auto text-center width-1of4" style="border: 1px solid blue">
+        <router-link :to="{ path: '/marca', params: {} }">
+          <i style="font-size: 48px;">account_circle</i>
+        </router-link>
+      </div>
+
+    </div>
+    <div class="list platform-delimiter item-inset-delimiter">
+      <div class="list-header">
+        Right Side Drawer
+      </div>
+      <q-drawer-link icon="mail" to="/shopping-cart">Shopping Cart</q-drawer-link>
+      <q-drawer-link icon="mail" to="/weather">Weather</q-drawer-link>
+    </div>
+  </q-drawer>
+
+  <div slot="footer" class="toolbar">
+    <slot name="footer">
+      &copy; MG Papelaria
+    </slot>
+  </div>
+
+</q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  props: {
+    navigation: {
+      type: Boolean,
+      default: false
+    },
+    drawer: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ mg-quasar/src/components/views/marca/Index.vue
@@ -0,0 +1,56 @@
+<template>
+  <!-- root node required -->
+  <mg-layout navigation drawer>
+
+    <!-- your content -->
+    <template slot="title">Marcas</template>
+
+    <template slot="navigation">
+      <q-tab icon="mail" route="/layout" exact replace>Mails</q-tab>
+      <q-tab icon="mail" route="/layout" exact replace>Mails</q-tab>
+      <q-tab icon="mail" route="/layout" exact replace>Mails</q-tab>
+      <q-tab icon="mail" route="/layout" exact replace>Mails</q-tab>
+      <q-tab icon="mail" route="/layout" exact replace>Mails</q-tab>
+      <q-tab icon="alarm" route="/layout/alarm" exact replace>Alarms</q-tab>
+      <q-tab icon="help" route="/layout/help" exact replace>Help</q-tab>
+    </template>
+
+    <template slot="drawer">
+      <div class="toolbar">
+        <q-toolbar-title>
+          Marcas
+        </q-toolbar-title>
+      </div>
+
+      <div class="list no-border platform-delimiter">
+        <q-drawer-link icon="mail" :to="{path: '/', exact: true}">
+          Link daqui
+        </q-drawer-link>
+      </div>
+    </template>
+
+    <div slot="content" class="layout-padding">
+      teste
+      <!-- if you want automatic padding -->
+    </div>
+
+
+  </mg-layout>
+</template>
+
+<script>
+
+import MgLayout from '../../layouts/MgLayout'
+
+export default {
+  data () {
+    return {}
+  },
+  components: {
+    MgLayout
+  }
+}
+</script>
+
+<style>
+</style>
