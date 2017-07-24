b1b5c40cba15dc0b53aa6c52be88e779e994ea40
Luciano Reis <lucianoreis.dev@gmail.com>
2017-07-24 17:07:01 -0400

    [UPD] Layout

--- mg-quasar-0.14/src/components/layouts/MgLayout.vue
+++ mg-quasar-0.14/src/components/layouts/MgLayout.vue
@@ -69,12 +69,6 @@
     </div>
   </q-drawer>
 
-  <div slot="footer" class="toolbar">
-    <slot name="footer">
-      &copy; MG Papelaria
-    </slot>
-  </div>
-
 </q-layout>
 -->
 <q-layout
@@ -85,9 +79,11 @@
   :reveal="reveal"
 >
   <q-toolbar slot="header">
-    <q-btn flat @click="$refs.layout.toggleLeft()">
-      <q-icon name="menu" />
-    </q-btn>
+    <slot name="menu">
+      <q-btn flat @click="$refs.layout.toggleLeft()" class="hide-on-drawer-visible">
+        <q-icon name="menu" />
+      </q-btn>
+    </slot>
 
     <q-toolbar-title>
       <slot name="title"></slot>
@@ -98,9 +94,13 @@
     <q-btn class="within-iframe-hide" flat @click="$router.replace('/showcase')" style="margin-right: 15px">
       <q-icon name="arrow_back" />
     </q-btn>
-    <q-btn flat @click="$refs.layout.toggleRight()">
-      <q-icon name="apps" />
-    </q-btn>
+
+    <slot name="menuRight">
+      <q-btn flat @click="$refs.layout.toggleRight()">
+        <q-icon name="apps" />
+      </q-btn>
+    </slot>
+
   </q-toolbar>
 
   <q-tabs slot="navigation" v-if="!hideTabs">
@@ -110,38 +110,18 @@
     <q-route-tab slot="title" icon="play_for_work" to="/showcase/layout/floating-action-button" replace label="Floating Action Button" />
   </q-tabs>
 
-  <q-scroll-area slot="left" style="width: 100%; height: 100%">
-    <q-list-header>Menu</q-list-header>
-    <q-side-link item to="/showcase/layout/play-with-layout">
-      <q-item-side icon="account circle" />
-      <q-item-main label="Play with Layout" sublabel="Learn more about it" />
-      <q-item-side right icon="thumb_up" />
-    </q-side-link>
-    <q-side-link item to="/showcase/layout/drawer-panels">
-      <q-item-side icon="view_array" />
-      <q-item-main label="Drawer Panels" sublabel="Layout left/right sides" />
-    </q-side-link>
-    <q-side-link item to="/showcase/layout/fixed-positioning">
-      <q-item-side icon="pin_drop" />
-      <q-item-main label="Fixed Positioning" sublabel="...on a Layout" />
-    </q-side-link>
-    <q-side-link item to="/showcase/layout/floating-action-button">
-      <q-item-side icon="play_for_work" />
-      <q-item-main label="Floating Action Button" sublabel="For Page actions" />
-    </q-side-link>
 
-    <!--
-    <div v-if="leftScroll" style="padding: 25px 16px 16px;">
-      <p class="caption" v-for="n in 50">
-        <em>Left Panel has intended scroll</em>
-      </p>
-    </div>
-    -->
 
+  <q-scroll-area slot="left" style="width: 100%; height: 100%"  v-if="drawer">
+    <slot name="drawer"></slot>
   </q-scroll-area>
 
   <q-scroll-area slot="right" style="width: 100%; height: 100%">
-    <q-list-header>Apps</q-list-header>
+    <q-side-link item>
+      <q-item-main label="Usuário" />
+      <q-item-side right icon="exit_to_app" />
+    </q-side-link>
+    </q-side-link>
     <q-side-link v-for="aplicativo in aplicativos" item :to="aplicativo.path" :key="aplicativo.title">
       <q-item-side :icon="aplicativo.icon" />
       <q-item-main :label="aplicativo.title" sublabel="Learn more about it" />
