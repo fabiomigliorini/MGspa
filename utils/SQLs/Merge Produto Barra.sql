f1159ab1e6d0686c2f5f0dfb1c6fc088005b8d6a
Fabio Migliorini <fabio@mgpapelaria.com.br>
2017-12-06 19:03:24 -0300

    [UPD] SQLs

--- utils/SQLs/Merge Produto Barra.sql
+++ utils/SQLs/Merge Produto Barra.sql
@@ -1,4 +1,4 @@
-﻿update tblnotafiscalprodutobarra    set codprodutobarra = 30022247    where codprodutobarra = 30022012  ;
-update tblnegocioprodutobarra       set codprodutobarra = 30022247    where codprodutobarra = 30022012  ;
-update tblcupomfiscalprodutobarra   set codprodutobarra = 30022247    where codprodutobarra = 30022012  ;
-update tblnfeterceiroitem           set codprodutobarra = 30022247    where codprodutobarra = 30022012  ;
+﻿update tblnotafiscalprodutobarra    set codprodutobarra = 959131    where codprodutobarra in (10013470, 10015034, 10013469, 10013471, 10015035) ;
+update tblnegocioprodutobarra       set codprodutobarra = 959131    where codprodutobarra in (10013470, 10015034, 10013469, 10013471, 10015035) ;
+update tblcupomfiscalprodutobarra   set codprodutobarra = 959131    where codprodutobarra in (10013470, 10015034, 10013469, 10013471, 10015035) ;
+update tblnfeterceiroitem           set codprodutobarra = 959131    where codprodutobarra in (10013470, 10015034, 10013469, 10013471, 10015035) ;
