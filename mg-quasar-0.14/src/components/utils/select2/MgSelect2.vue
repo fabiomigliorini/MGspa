23684aeb869134010aedd8e566687cd81bc80511
Luciano Reis <lucianoreis.dev@gmail.com>
2017-12-12 17:24:46 -0300

    [ADD] Autocomplete reutilizavel

---
+++ mg-quasar-0.14/src/components/utils/select2/MgSelect2.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="autocomplete-input">
+      <p class="control has-icon has-icon-right">
+        <input
+          v-model="keyword"
+          class="input is-large"
+          placeholder="Search..."
+          @input="onInput($event.target.value)"
+          @keyup.esc="isOpen = false"
+          @blur="isOpen = false"
+          @keydown.down="moveDown"
+          @keydown.up="moveUp"
+          @keydown.enter="select"
+        >
+        <i class="fa fa-angle-down"></i>
+      </p>
+      <ul v-show="isOpen"
+        class="options-list"
+      >
+        <li v-for="(option, index) in fOptions"
+          :class="{
+            'highlighted': index === highlightedPosition
+          }"
+          @mouseenter="highlightedPosition = index"
+          @mousedown="select"
+        >
+          <slot name="item"
+            :title="option.title"
+            :description="option.description"
+            :thumbnail="option.thumbnail"
+          ></slot>
+        </li>
+      </ul>
+    </div>
+  </template>
+
+<script>
+  // import { QSelect } from 'quasar'
+  export default {
+    name: 'mg-select2',
+    props: {
+      options: {
+        type: Array,
+        required: true
+      }
+    },
+    data () {
+      return {
+        isOpen: false,
+        highlightedPosition: 0,
+        keyword: ''
+      }
+    },
+    computed: {
+      fOptions () {
+        const re = new RegExp(this.keyword, 'i')
+        return this.options.filter(o => o.title.match(re))
+      }
+    },
+    methods: {
+      onInput (value) {
+        this.highlightedPosition = 0
+        this.isOpen = !!value
+      },
+      moveDown () {
+        if (!this.isOpen) {
+          return
+        }
+        this.highlightedPosition =
+          (this.highlightedPosition + 1) % this.fOptions.length
+      },
+      moveUp () {
+        if (!this.isOpen) {
+          return
+        }
+        this.highlightedPosition = this.highlightedPosition - 1 < 0
+          ? this.fOptions.length - 1
+          : this.highlightedPosition - 1
+      },
+      select () {
+        const selectedOption = this.fOptions[this.highlightedPosition]
+        this.$emit('select', selectedOption)
+        this.isOpen = false
+        this.keyword = selectedOption.title
+      }
+    }
+  }
+</script>
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+.autocomplete-input {
+  position: relative;
+}
+
+ul.options-list {
+  display: flex;
+  flex-direction: column;
+  margin-top: -12px;
+  border: 1px solid #dbdbdb;
+  border-radius: 0 0 3px 3px;
+  position: absolute;
+  width: 100%;
+  overflow: hidden;
+}
+
+ul.options-list li {
+  width: 100%;
+  flex-wrap: wrap;
+  background: white;
+  margin: 0;
+  border-bottom: 1px solid #eee;
+  color: #363636;
+  padding: 7px;
+  cursor: pointer;
+}
+
+ul.options-list li.highlighted {
+  background: #f8f8f8
+}
+</style>
--- mg-quasar-0.14/src/components/utils/select2/MgSelect2Pessoa.vue
+++ mg-quasar-0.14/src/components/utils/select2/MgSelect2Pessoa.vue
@@ -1,122 +1,52 @@
 <template>
-    <!-- <select :placeholder="placeholder" style="width:100%">
-      <option value="pessoa.codpessoa" selected="selected" v-if="pessoa">{{ pessoa.pessoa }}</option>
-    </select> -->
+<mg-select2 :options="options" @select="onOptionSelect">
+  <template slot="item" scope="option">
+      <p>
+        <strong>{{ option.title }}</strong>
+        <br> <small>{{ option.description }}</small>
+      </p>
+    </template>
+</mg-select2>
 </template>
 <script>
-export default {
-  name: 'mg-select2-pessoa'
-}
-/*
-import $ from 'jquery'
-import 'select2'
-
+import MgSelect2 from './MgSelect2'
 export default {
   name: 'mg-select2-pessoa',
-  deep: true,
-  props: ['value', 'placeholder'],
+  components: {
+    MgSelect2
+  },
   data () {
     return {
-      pessoa: false
-    }
-  },
-  watch: {
-    value: function (value) {
-      $(this.$el).val(value).trigger('change')
-      console.log('Mudou o valor')
-    }
-  },
-  mounted: function () {
-    let vm = this
-
-    if (vm.value) {
-      let params = {
-        id: vm.value
-      }
-      window.axios.get('pessoa/select2', { params }).then(function (request) {
-        vm.pessoa = request.data
-      }).catch(function (error) {
-        console.log(error.response)
-      })
-    }
-
-    $(this.$el)
-      .select2({
-        placeholder: vm.placeholder,
-        language: 'pt-BR',
-        minimumInputLength: 2,
-        allowClear: true,
-        ajax: {
-          headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-            Authorization: `Bearer ${localStorage.getItem('auth.token')}`
-          },
-          url: process.env.API_BASE_URL + 'pessoa/select2',
-          dataType: 'json',
-          delay: 250,
-          data: function (params) {
-            return {
-              q: params.term,
-              page: params.page
-            }
-          },
-          processResults: function (data, params) {
-            params.page = params.page || 1
-
-            return {
-              results: data.items,
-              pagination: {
-                more: (params.page * 30) < data.total_count
-              }
-            }
-          },
-          cache: true
+      options: [
+        {
+          title: 'First Scene',
+          description: 'lorem ipsum dolor amet.',
+          thumbnail: 'http://lorempicsum.com/nemo/200/200/1'
         },
-        escapeMarkup: function (markup) {
-          return markup
+        {
+          title: 'Second Scene',
+          description: 'lorem ipsum dolor amet.',
+          thumbnail: 'http://lorempicsum.com/nemo/200/200/2'
         },
-        templateResult: formatData,
-        templateSelection: formatDataSelection
-      })
-      .trigger('change')
-        .on('change', function () {
-          vm.$emit('input', this.value)
-        })
-
-    function formatData (data) {
-      if (data.loading) {
-        return data.text
-      }
-
-      var markup = '' +
-        '<div class="q-item-main q-item-section">' +
-          '<div class="q-item-label">' +
-            data.fantasia + '<div class="right q-item-stamp">' + data.id + '</div>' +
-          '</div>' +
-          '<div class="q-item-sublabel">' +
-            data.pessoa + '<div class="right q-item-stamp">' + data.cnpj + '</div>' +
-          '</div>'
-
-      if (data.inativo) {
-        markup += '<div class="text-negative">' +
-          'inativo desde ' + data.inativo +
-        '</div>'
-      }
-
-      markup += '</div>'
-
-      return markup
-    }
-
-    function formatDataSelection (data) {
-      return data.fantasia || data.text
+        {
+          title: 'Third Scene',
+          description: 'lorem ipsum dolor amet.',
+          thumbnail: 'http://lorempicsum.com/nemo/200/200/3'
+        },
+        {
+          title: 'Fourth Scene',
+          description: 'lorem ipsum dolor amet.',
+          thumbnail: 'http://lorempicsum.com/nemo/200/200/4'
+        }
+      ]
     }
   },
-  destroyed: function () {
-    $(this.$el).off().select2('destroy')
+  methods: {
+    onOptionSelect (option) {
+      console.log('Selected option:', option)
+    }
   }
 }
-*/
 </script>
 
 <style>
