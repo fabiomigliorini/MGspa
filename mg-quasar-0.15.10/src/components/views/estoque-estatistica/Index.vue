9d76f5b21bfbdff07fad4af8804dc01fec2d20b5
Atanael Mendes Gamarra <atanael@mgpapelaria.com.br>
2018-06-07 18:14:22 -0400

    [UPD] ajustes na tela de estatistica

--- mg-quasar-0.15.10/src/components/views/estoque-estatistica/Index.vue
+++ mg-quasar-0.15.10/src/components/views/estoque-estatistica/Index.vue
@@ -51,98 +51,182 @@
     <!-- Conteúdo Princial (Meio) -->
     <div slot="content">
 
-      <q-card>
-        <q-card-title>
-          Venda Mensal
-          <span slot="subtitle">
-            Quantidade vendida mês à mês, comparada com o saldo atual do estoque.
-            O gráfico de área mostra as vendas entre Janeiro e Março de cada ano, comparadas com o saldo atual do estoque.
-          </span>
-        </q-card-title>
-        <q-card-separator />
-        <q-card-main class="row">
-          <div class="col-md-6">
-            <grafico-vendas-geral :height=350 :meses="meses" :vendas="item.vendas" :saldoquantidade="item.saldoquantidade"></grafico-vendas-geral>
-          </div>
-          <div class="col-md-3">
-            <grafico-volta-aulas :height="350" :vendas="item.vendas_volta_aulas" :saldoquantidade="item.saldoquantidade"></grafico-volta-aulas>
-          </div>
-          <div class="col-md-3">
-            <template v-if="item">
-              <h5>Estatísticas</h5>
-              <p>Demanda média: {{ numeral(item.estatistica.demandamedia).format('0,0.0000') }}</p>
-              <p>Desvio padrao: {{ numeral(item.estatistica.desviopadrao).format('0,0.0000') }}</p>
-              <p>Nível de servico: {{ numeral(item.estatistica.nivelservico).format('0%') }}</p>
-              <p>Estoque de segurança: {{ numeral(item.estatistica.estoqueseguranca).format('0,0') }}</p>
-              <p>Estoque mínimo: {{ numeral(item.estatistica.estoqueminimo).format('0,0') }} ({{ numeral(item.estatistica.tempominimo * 30).format('0,0') }} Dias)</p>
-              <p>Estoque máximo: {{ numeral(item.estatistica.estoquemaximo).format('0,0') }} ({{ numeral(item.estatistica.tempomaximo * 30).format('0,0') }} Dias)</p>
-              <p>Estoque quantidade: {{ numeral(item.saldoquantidade).format('0,0') }}</p>
-              <p>Sugestão de compra: {{ numeral(item.estatistica.estoquemaximo - item.saldoquantidade).format('0,0') }}</p>
-            </template>
-          </div>
-        </q-card-main>
-        <q-card-actions>
-          <span slot="subtitle">
-            <q-btn @click="meses=null" :color="(meses == null)?'primary':''" flat>Desde Início</q-btn>
-            <q-btn @click="meses=36" :color="(meses == 36)?'primary':''" flat>3 Anos</q-btn>
-            <q-btn @click="meses=24" :color="(meses == 24)?'primary':''" flat>2 Anos</q-btn>
-            <q-btn @click="meses=12" :color="(meses == 12)?'primary':''" flat>1 Ano</q-btn>
-            <q-btn @click="meses=6" :color="(meses == 6)?'primary':''" flat>6 meses</q-btn>
-          </span>
-        </q-card-actions>
-      </q-card>
+      <div>
 
-        <!-- <div class="col-md-3">
-          <q-card>
-            <q-card-title>
-              Volta às aulas
-              <span slot="subtitle">Vendas entre Janeiro e Março de cada ano, comparadas com o saldo atual do estoque.</span>
-            </q-card-title>
-            <q-card-separator />
-            <q-card-main>
+        <div>
+          <q-card >
+            <div style="padding-left:10px; padding-top:10px">
+              <strong>Venda Mensal</strong>
+              <q-icon name="live_help" color="primary" size="25px">
+                <q-tooltip anchor="bottom left" self="top right">
+                  Quantidade vendida mês à mês,
+                  comparada com o saldo atual do estoque.<br />
+                  O gráfico de área mostra as vendas entre Janeiro e Março de cada ano,
+                  comparadas com o saldo atual do estoque.
+                </q-tooltip>
+              </q-icon>
+            </div>
+            <q-card-main class="row">
+              <div class="col-md-6">
+                <grafico-vendas-geral :height="200" :meses="meses" :vendas="item.vendas" :saldoquantidade="item.saldoquantidade"></grafico-vendas-geral>
+              </div>
+              <div class="col-md-3">
+                <grafico-volta-aulas :height="200"  :vendas="item.vendas_volta_aulas" :saldoquantidade="item.saldoquantidade"></grafico-volta-aulas>
+              </div>
+
+              <div class="col-md-3" style="height:200px">
+
+                <template v-if="item">
+                  <q-list dense no-border>
+                    <q-item>
+                      <q-item-side >
+                        Média:
+                      </q-item-side>
+                      <q-item-main align="end">
+                        {{ numeral(item.estatistica.demandamedia).format('0,0.0000') }}
+                      </q-item-main>
+                    </q-item>
+
+                    <q-item>
+                      <q-item-side>
+                        Desvio:
+                      </q-item-side>
+                      <q-item-main align="end">
+                        {{ numeral(item.estatistica.desviopadrao).format('0,0.0000') }}
+                      </q-item-main>
+                    </q-item>
+
+                    <q-item>
+                      <q-item-side>
+                        Servico:
+                      </q-item-side>
+                      <q-item-main align="end">
+                        {{ numeral(item.estatistica.nivelservico).format('0%') }}
+                      </q-item-main>
+                    </q-item>
+
+                    <q-item>
+                      <q-item-side>
+                        Segurança:
+                      </q-item-side>
+                      <q-item-main align="end">
+                        {{ numeral(item.estatistica.estoqueseguranca).format('0,0') }}
+                      </q-item-main>
+                    </q-item>
+
+                    <q-item>
+                      <q-item-side>
+                        Mínimo:
+                      </q-item-side>
+                      <q-item-main align="end">
+                        {{ numeral(item.estatistica.estoqueminimo).format('0,0') }} ({{ numeral(item.estatistica.tempominimo * 30).format('0,0') }} Dias)
+                      </q-item-main>
+                    </q-item>
+
+                    <q-item>
+                      <q-item-side>
+                        Máximo:
+                      </q-item-side>
+                      <q-item-main align="end">
+                        {{ numeral(item.estatistica.estoquemaximo).format('0,0') }} ({{ numeral(item.estatistica.tempomaximo * 30).format('0,0') }} Dias)
+                      </q-item-main>
+                    </q-item>
+
+                    <q-item>
+                      <q-item-side>
+                        Saldo:
+                      </q-item-side>
+                      <q-item-main align="end">
+                        {{ numeral(item.saldoquantidade).format('0,0') }}
+                      </q-item-main>
+                    </q-item>
+
+                    <q-item>
+                      <q-item-side>
+                        Comprar:
+                      </q-item-side>
+                      <q-item-main align="end">
+                        {{ numeral(item.estatistica.estoquemaximo - item.saldoquantidade).format('0,0') }}
+                      </q-item-main>
+                    </q-item>
+
+                  </q-list>
+                </template>
+
+                <!-- <template v-if="item">
+                  <h5>Estatísticas</h5>
+                  <p>Demanda média: {{ numeral(item.estatistica.demandamedia).format('0,0.0000') }}</p>
+                  <p>Desvio padrao: {{ numeral(item.estatistica.desviopadrao).format('0,0.0000') }}</p>
+                  <p>Nível de servico: {{ numeral(item.estatistica.nivelservico).format('0%') }}</p>
+                  <p>Estoque de segurança: {{ numeral(item.estatistica.estoqueseguranca).format('0,0') }}</p>
+                  <p>Estoque mínimo: {{ numeral(item.estatistica.estoqueminimo).format('0,0') }} ({{ numeral(item.estatistica.tempominimo * 30).format('0,0') }} Dias)</p>
+                  <p>Estoque máximo: {{ numeral(item.estatistica.estoquemaximo).format('0,0') }} ({{ numeral(item.estatistica.tempomaximo * 30).format('0,0') }} Dias)</p>
+                  <p>Estoque quantidade: {{ numeral(item.saldoquantidade).format('0,0') }}</p>
+                  <p>Sugestão de compra: {{ numeral(item.estatistica.estoquemaximo - item.saldoquantidade).format('0,0') }}</p>
+                </template> -->
+              </div>
+
             </q-card-main>
+            <q-card-actions>
+              <span>
+                <q-btn @click.native="meses=null" :color="(meses == null)?'primary':''" flat>Desde Início</q-btn>
+                <q-btn @click.native="meses=36" :color="(meses == 36)?'primary':''" flat>3 Anos</q-btn>
+                <q-btn @click.native="meses=24" :color="(meses == 24)?'primary':''" flat>2 Anos</q-btn>
+                <q-btn @click.native="meses=12" :color="(meses == 12)?'primary':''" flat>1 Ano</q-btn>
+                <q-btn @click.native="meses=6" :color="(meses == 6)?'primary':''" flat>6 meses</q-btn>
+              </span>
+            </q-card-actions>
           </q-card>
         </div>
-        <div class="col-md-3">
-          <q-card>
-            <q-card-title>
-              Estatísticas
-            </q-card-title>
-            <q-card-separator />
-          </q-card>
-        </div>
-      </div> -->
 
-      <q-card>
-        <q-card-title>
-          Filiais
-          <span slot="subtitle">
-            Vendas dos últimos 12 meses de cada filial, comparadas com o saldo atual dos estoques.
-            O gráfico em formato de anel mostra a distribuição da venda dos últimos 12 meses comparado com os estoques.
-            O anel externo representa as vendas, já o interno representa os saldos atuais de estoque.
-          </span>
-        </q-card-title>
-        <q-card-separator />
-        <q-card-main class="row">
-          <div  class="col-md-8">
-            <grafico-vendas-ano-filiais :height="350" :locais="item.locais" :vendaquantidade="item.vendaquantidade" :saldoquantidade="item.saldoquantidade"></grafico-vendas-ano-filiais>
+        <q-card>
+          <div style="padding-left:10px; padding-top:10px">
+            <strong>Filiais</strong>
+            <q-icon name="live_help" color="primary" size="25px">
+              <q-tooltip anchor="bottom left" self="top right">
+                Vendas dos últimos 12 meses de cada filial, comparadas com o saldo atual dos estoques.<br />
+                O gráfico em formato de anel mostra a distribuição da venda dos últimos 12 meses comparado com os estoques.<br />
+                O anel externo representa as vendas, já o interno representa os saldos atuais de estoque.
+              </q-tooltip>
+            </q-icon>
           </div>
-          <div  class="col-md-4">
-            <grafico-vendas-estoque-filiais :height="350" :locais="item.locais"></grafico-vendas-estoque-filiais>
-          </div>
-        </q-card-main>
-      </q-card>
-
+          <q-card-main class="row">
+            <div  class="col-md-8">
+              <grafico-vendas-ano-filiais :height="200" :locais="item.locais" :vendaquantidade="item.vendaquantidade" :saldoquantidade="item.saldoquantidade"></grafico-vendas-ano-filiais>
+            </div>
+            <div  class="col-md-4">
+              <grafico-vendas-estoque-filiais :height="200" :locais="item.locais"></grafico-vendas-estoque-filiais>
+            </div>
+          </q-card-main>
+        </q-card>
+      </div>
       <template v-if="item && item.variacoes.length > 1">
-        <variacoes :height="350" :variacoes="item.variacoes"></variacoes>
+        <variacoes :variacoes="item.variacoes"></variacoes>
       </template>
     </div>
-
-    <div slot="footer">
-    </div>
-
   </mg-layout>
 </template>
+
+<!-- <div class="col-md-3">
+<q-card>
+<q-card-title>
+Volta às aulas
+<span slot="subtitle">Vendas entre Janeiro e Março de cada ano, comparadas com o saldo atual do estoque.</span>
+</q-card-title>
+<q-card-separator />
+<q-card-main>
+</q-card-main>
+</q-card>
+</div>
+<div class="col-md-3">
+<q-card>
+<q-card-title>
+Estatísticas
+</q-card-title>
+<q-card-separator />
+</q-card>
+</div>
+</div> -->
 
 <script>
 
--- mg-quasar-0.15.10/src/components/views/estoque-estatistica/grafico-estatistica.vue
+++ mg-quasar-0.15.10/src/components/views/estoque-estatistica/grafico-estatistica.vue
@@ -6,22 +6,28 @@ import {
 import { debounce } from 'quasar'
 
 export default {
-  name: 'grafico-vendas-ano-filiais',
+  name: 'grafico-estatistica',
   extends: Bar,
-  props: ['locais', 'saldoquantidade', 'vendaquantidade'],
+  // props: ['locais', 'saldoquantidade', 'vendaquantidade'],
+  props: ['seguranca', 'minimo', 'maximo'],
   data () {
     return {
       data: {
-        labels: [],
+        labels: ['Estatistica'],
         datasets: [
           {
-            label: 'Vendas',
-            backgroundColor: 'rgba(63, 81, 181, 0.7)',
+            label: 'Máximo',
+            backgroundColor: 'rgba(0,128,0, 0.7)',
+            data: null,
+          },
+          {
+            label: 'Minimo',
+            backgroundColor: 'rgba(255,165,0, 0.7)',
             data: null
           },
           {
-            label: 'Estoque',
-            backgroundColor: '#f00',
+            label: 'Segurança',
+            backgroundColor: 'rgba(255,0,0, 0.7)',
             data: null
           }
         ]
@@ -31,9 +37,13 @@ export default {
         maintainAspectRatio: false,
         scales: {
           yAxes: [{
+            stacked: true,
             ticks: {
               beginAtZero: true
             }
+          }],
+          yAxes: [{
+             stacked: true // this also..
           }]
         }
       }
@@ -51,6 +61,8 @@ export default {
     this.renderChart(this.data, this.options)
   },
   methods: {
+
+
     update () {
       this.$data._chart.update()
     },
@@ -59,9 +71,12 @@ export default {
       let vm = this
 
       // acumula dados para os datasets
-      let locais = []
-      let vendaquantidade = []
-      let saldoquantidade = []
+      let maximo = []
+      let minimo = []
+      let seguranca = []
+      // let locais = []
+      // let vendaquantidade = []
+      // let saldoquantidade = []
 
       this.locais.forEach(function (estoquelocal) {
         locais.push(estoquelocal.estoquelocal.substr(0, 3))
@@ -74,9 +89,10 @@ export default {
       saldoquantidade.push(vm.saldoquantidade)
 
       // passa para datasets os valores acumulados
-      vm.data.datasets[0].data = vendaquantidade
-      vm.data.datasets[1].data = saldoquantidade
-      vm.data.labels = locais
+      vm.data.datasets[0].data = maximo
+      vm.data.datasets[1].data = minimo
+      vm.data.datasets[2].data = seguranca
+      vm.data.labels = estatistica
 
       // atualiza grafico
       vm.update()
