cfe4f04e86001cc4a762bb483ca2b21f4092d38f
Fabio Migliorini <fabio@mgpapelaria.com.br>
2018-12-18 12:41:00 -0300

    [UPD] Validacao Endereco Destinatario no Email da NFe

--- laravel/app/Mg/NFePHP/NFePHPRepositoryMail.php
+++ laravel/app/Mg/NFePHP/NFePHPRepositoryMail.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Mail;
 
 use Mg\NFePHP\Mail\NFeAutorizadaMail;
 use Mg\NotaFiscal\NotaFiscal;
+use Validator;
 
 class NFePHPRepositoryMail
 {
@@ -13,51 +14,55 @@ class NFePHPRepositoryMail
     public static function mail(NotaFiscal $nf, $destinatario = null)
     {
 
-        if ($nf->Filial->nfeambiente == 2) {
-            return [
-              'sucesso' => true,
-              'mensagem' => 'Ignorado envio - BASE HOMOLOGACAO',
-              'destinatario' => $destinatario,
-            ];
-        }
-
+        // se destinatario veio em branco, busca do cadastro
         if (empty($destinatario)) {
             $destinatario = $nf->Pessoa->emailnfe??$nf->Pessoa->email??$nf->Pessoa->emailcobranca;
         }
 
-        if (empty($destinatario)) {
-            return [
-              'sucesso' => true,
-              'mensagem' => 'Nenhum endereço de E-mail informado!',
-              'destinatario' => $destinatario,
-            ];
+        // transforma em array
+        if (strpos($destinatario, ',')) {
+            $destinatarios = explode(',', $destinatario);
+        } else {
+            $destinatarios = [$destinatario];
         }
 
+        // percorre array validando todos os enderecos
+        foreach ($destinatarios as $key => $destinatario) {
+            $destinatario = trim($destinatario);
+            $destinatarios[$key] = $destinatario;
+            $validator = Validator::make(['destinatario'=>$destinatario], [
+                'destinatario' => 'required|email'
+            ]);
+            if ($validator->fails()) {
+                $erros = $validator->errors();
+                return [
+                    'sucesso' => false,
+                    'mensagem' => $erros->first(),
+                    'destinatario' => $destinatarios,
+                    'error' => $erros
+                ];
+            }
+        }
+
+        // verifica se a nota fiscal é valida
         if (empty($nf->nfeautorizacao)) {
             throw new \Exception('Esta NFe não está autorizada!');
         }
-
         if (!empty($nf->nfecancelamento)) {
             throw new \Exception('Esta NFe está cancelada!');
         }
-
         if (!empty($nf->nfeinutilizacao)) {
             throw new \Exception('Esta NFe está inutilizada!');
         }
 
-        if (strpos($destinatario, ',')) {
-            $destinatario = explode(',', $destinatario);
-        } else {
-            $destinatario = [$destinatario];
-        }
+        // envia o email
+        Mail::to($destinatarios)->queue(new NFeAutorizadaMail($nf));
 
-        //return new NFeAutorizadaMail($nf);
-        Mail::to($destinatario)->queue(new NFeAutorizadaMail($nf));
-
+        // retorna o sucesso na execucao
         return [
           'sucesso' => true,
-          'mensagem' => 'Email enviado para: ' . implode(', ', $destinatario),
-          'destinatario' => $destinatario,
+          'mensagem' => 'Email enviado para: ' . implode(', ', $destinatarios),
+          'destinatario' => $destinatarios,
         ];
     }
 
