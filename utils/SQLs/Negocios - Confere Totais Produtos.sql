7b956c6ba475c1382c7be8fa1220a05a431081f0
Fabio Migliorini <fabio.migliorini@gmail.com>
2020-01-01 17:57:41 -0400

    [ADD] SQLs

---
+++ utils/SQLs/Negocios - Confere Totais Produtos.sql
@@ -0,0 +1,14 @@
+﻿with tot as (
+	select nfpb.codnegocio, sum(nfpb.valortotal) as total
+	from tblnegocioprodutobarra nfpb
+	group by nfpb.codnegocio
+) 
+select 
+	nf.codnegocio,
+	nf.valorprodutos,
+	tot.total,
+	nf.*
+from tblnegocio nf
+left join tot on (tot.codnegocio = nf.codnegocio)
+where nf.valorprodutos != coalesce(tot.total, 0)
+--limit 50
---
+++ utils/SQLs/Nota Fiscal - Confere Totais Produtos.sql
@@ -0,0 +1,14 @@
+﻿with tot as (
+	select nfpb.codnotafiscal, sum(nfpb.valortotal) as total
+	from tblnotafiscalprodutobarra nfpb
+	group by nfpb.codnotafiscal
+) 
+select 
+	nf.codnotafiscal,
+	nf.valorprodutos,
+	tot.total,
+	nf.*
+from tblnotafiscal nf
+left join tot on (tot.codnotafiscal = nf.codnotafiscal)
+where nf.valorprodutos != tot.total
+limit 50
