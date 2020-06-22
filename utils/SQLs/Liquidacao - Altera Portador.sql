2b8b19f3afa514192acb1153737044bfd6a8906d
Fabio Migliorini <fabio.migliorini@gmail.com>
2020-06-22 10:42:21 -0400

    [UPD] SQLs

--- utils/SQLs/Liquidacao - Altera Portador.sql
+++ utils/SQLs/Liquidacao - Altera Portador.sql
@@ -1,7 +1,9 @@
-﻿update tblliquidacaotitulo set codportador = 202008 where codliquidacaotitulo = 50808
+﻿update tblliquidacaotitulo set codportador = 00000004 where codliquidacaotitulo = 63840
 
 update tblmovimentotitulo
 set codportador = lt.codportador
 from tblliquidacaotitulo lt
 where lt.codliquidacaotitulo = tblmovimentotitulo.codliquidacaotitulo
 and lt.codportador != tblmovimentotitulo.codportador
+
+commit
