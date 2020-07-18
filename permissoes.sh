d215cff7b1635f3e29ae9e799164469f090cfc7c
Fabio Migliorini <fabio.migliorini@gmail.com>
2020-07-18 13:59:19 -0400

    [FIX] Permissoes

--- permissoes.sh
+++ permissoes.sh
@@ -3,3 +3,6 @@ find /opt/www/MGspa -type d -exec chmod 775 {} \;
 find /opt/www/MGspa -type f -exec chmod 664 {} \;
 chmod a+w /opt/www/MGspa/laravel/storage -R
 chmod a+w /opt/www/NFePHP -R
+chmod +x /opt/www/MGspa/shell
+chmod +x /opt/www/MGspa/start
+chmod +x /opt/www/MGspa/stop
