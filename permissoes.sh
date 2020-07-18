618f0e15f41235328f6cb2c5b8d447daf8c44c24
Fabio Migliorini <fabio.migliorini@gmail.com>
2020-07-18 14:02:15 -0400

    [FIX] Permissoes

--- permissoes.sh
+++ permissoes.sh
@@ -5,8 +5,5 @@ chmod a+w /opt/www/MGspa/laravel/storage -R
 chmod a+w /opt/www/NFePHP -R
 chmod +x /opt/www/MGspa/shell
 chmod +x /opt/www/MGspa/start
-<<<<<<< HEAD
 chmod +x /opt/www/MGspa/stop
-=======
-chmox +x /opt/www/MGspa/stop
->>>>>>> 6077b113759c9a6d0b644b332086a0952ec0816a
+chmod +x /opt/www/MGspa/copiar-imagens
