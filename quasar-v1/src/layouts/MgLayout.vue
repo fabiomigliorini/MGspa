fb73f20999599ecfa1d6f8ade4d439f75b20632a
Fabio Migliorini <fabio.migliorini@gmail.com>
2020-07-23 17:57:35 -0400

    [FIX] Layout Principal

--- quasar-v1/src/layouts/MgLayout.vue
+++ quasar-v1/src/layouts/MgLayout.vue
@@ -1,10 +1,10 @@
 <template>
-  <q-layout :view="view">
-    <q-header v-model="header" :reveal="headerReveal">
+  <q-layout view="hHr LpR fFr">
+    <q-header elevated reveal>
       <q-ajax-bar size="3px"/>
       <q-toolbar color="primary">
         <slot name="menu">
-          <q-btn flat round dense icon="menu" @click="left = !left" v-if="drawer"/>
+          <q-btn flat round dense icon="menu" @click="leftDrawer = !leftDrawer" v-if="drawer"/>
         </slot>
 
         <q-toolbar-title>
@@ -16,7 +16,7 @@
         </q-btn>
 
         <slot name="menuRight">
-          <q-btn flat round dense icon="apps" @click="rightSide = !rightSide" />
+          <q-btn flat round dense icon="apps" @click="rightDrawer = !rightDrawer" />
         </slot>
       </q-toolbar>
 
@@ -24,33 +24,19 @@
 
     </q-header>
 
-    <!-- <q-layout-footer v-model="footer" :reveal="footerReveal">
-      <q-toolbar>
-        <slot name="footer">
-          <q-toolbar-title>
-            © MG Papelaria
-          </q-toolbar-title>
-        </slot>
-      </q-toolbar>
-    </q-layout-footer> -->
-
     <!-- Left Side Panel -->
     <q-drawer
-      bordered
+      elevated
       v-if="drawer"
       side="left"
-      v-model="left"
-      :overlay="leftOverlay"
-      :behavior="leftBehavior"
-      :breakpoint="leftBreakpoint"
+      v-model="leftDrawer"
+      no-swipe-backdrop
     >
-      <q-scroll-area class="fit">
-        <slot name="drawer"></slot>
-      </q-scroll-area>
+      <slot name="drawer"></slot>
     </q-drawer>
 
     <!-- Right Side Panel -->
-    <q-drawer v-model="rightSide" side="right" behavior="mobile" bordered>
+    <q-drawer v-model="rightDrawer" side="right" behavior="mobile" bordered>
 
       <q-item>
         <q-item-section avatar link to="/inbox/1" v-if="perfil.avatar">
@@ -75,35 +61,14 @@
       <q-separator/>
 
 
-
-      <!-- <q-list inset-separator>
-        <q-item>
-          <q-item-side :avatar="perfil.avatar" v-if="perfil.avatar.length > 0"/>
-          <q-item-main>
-            <router-link :to="{ path: '/usuario/perfil' }">
-            {{ perfil.usuario }}
-            </router-link>
-          </q-item-main>
-          <q-item-side right icon="exit_to_app" @click="logout" style="cursor:pointer"/>
-        </q-item>
-      </q-list> -->
-
       <div class="row">
         <div class="text-center col-4" v-for="aplicativo in aplicativos">
 
           <q-item @click.native="$router.push(aplicativo.path)" style="cursor:pointer">
 
-            <!--<q-item-section avatar>-->
-              <!--<q-icon :name="aplicativo.icon" color="primary"/>-->
-            <!--</q-item-section>-->
-            <!--<q-item-section>-->
-              <!--<small class="text-primary">{{ aplicativo.title }}</small>-->
-            <!--</q-item-section>-->
-
-
             <q-item-section>
               <q-item-label>
-                <q-icon size="25px" :name="aplicativo.icon" color="primary"/>
+                <q-icon size="50px" :name="aplicativo.icon" color="primary"/>
               </q-item-label>
               <q-item-label caption class="text-primary">
                 {{ aplicativo.title }}
@@ -123,22 +88,11 @@
      </slot>
     </q-page-container>
 
-    <!-- <q-toolbar>
-      <q-toolbar-title>
-        <slot name="footer">
-          &copy; MG Papelaria
-        </slot>
-      </q-toolbar-title>
-    </q-toolbar> -->
-
-    <!-- Footer -->
-    <!--
-    <q-toolbar>
-    <q-toolbar-title>
-    &copy; MG Papelaria
-  </q-toolbar-title>
-</q-toolbar>
--->
+    <q-footer elevated reveal class="bg-grey-8 text-white">
+      <div class="q-ma-xs text-weight-light text-center">
+        MGspa - &copy; MG Papelaria
+      </div>
+    </q-footer>
 
   </q-layout>
 </template>
@@ -148,44 +102,8 @@ export default {
   name: 'mg-layout',
   data () {
     return {
-
-      view: 'lHr LpR lFr',
-      header: true,
-      headerReveal: true,
-
-      footer: false,
-      footerReveal: false,
-
-      left: true,
-      leftOverlay: false,
-      leftBreakpoint: 996,
-      leftBehavior: 'default',
-
-      leftScroll: false,
-      rightScroll: true,
-      rightBreakpoint: 2000,
-      hideTabs: true,
-      leftSide: false,
-      rightSide: false,
-
-      // right: true,
-      //
-      // headerReveal: true,
-      // rightOverlay: false,
-      // rightBehavior: 'default',
-      // leftBreakpoint: 992,
-      // rightBreakpoint: 992,
-      //
-      // topleft: 'h',
-      // topcenter: 'H',
-      // topright: 'h',
-      // middleleft: 'L',
-      // middlecenter: 'p',
-      // middleright: 'r',
-      // bottomleft: 'l',
-      bottomcenter: 'F',
-      bottomright: 'f',
-      scrolling: true
+      leftDrawer: false,
+      rightDrawer: false,
     }
   },
   components: {
@@ -203,10 +121,6 @@ export default {
     }
   },
   props: {
-    navigation: {
-      type: Boolean,
-      default: false
-    },
     drawer: {
       type: Boolean,
       default: false
@@ -244,10 +158,4 @@ export default {
 </script>
 
 <style>
-.icone-app {
-  font-size: 50px
-}
-.space-end{
-  margin-bottom: 100px;
-}
 </style>
