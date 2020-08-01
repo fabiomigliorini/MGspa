e71e07f45d777cdde144720ffaf1bf1dcced7cba
Fabio Migliorini <fabio.migliorini@gmail.com>
2020-08-01 16:51:10 -0400

    [ADD] RollUp Table ProdutoBarra

--- laravel/app/Mg/Select/SelectProdutoBarraController.php
+++ laravel/app/Mg/Select/SelectProdutoBarraController.php
@@ -12,80 +12,64 @@ class SelectProdutoBarraController extends Controller
 {
     public static function index(Request $request)
     {
-        $texto = $request->busca??'';
-        $pagina = $request->page??'';
+        $busca = $request->busca??'';
+        $page = $request->page??1;
         $limite = $request->limit??50;
+        $offset = ($page-1)*$limite;
+        // return $offset;
 
-        // limpa texto
-        $ordem = (strstr($texto, '$'))?'preco ASC, descricao ASC':'descricao ASC, preco ASC';
-        $texto = str_replace('$', '', $texto);
-        $texto  = str_replace(' ', '%', trim($texto));
+        // decide ordem
+        $ordem = (strstr($busca, '$'))?'preco ASC, descricao ASC':'descricao ASC, preco ASC';
+        $busca = str_replace('$', '', $busca);
 
-        // corrige pagina se veio sujeira
-        if ($pagina < 1) {
-            $pagina = 1;
+        $sql = "SELECT codprodutobarra, codproduto, barras, descricao, sigla, preco, marca, referencia, inativo
+                  FROM vwProdutoBarra
+                 WHERE codProdutoBarra is not null ";
+
+        if (!filter_var($request->inativo, FILTER_VALIDATE_BOOLEAN)) {
+            $sql .= "AND Inativo is not null ";
         }
 
-        // calcula de onde continuar a consulta
-        $offset = ($pagina-1)*$limite;
-
-        // inicializa array com resultados
-        $resultados = array();
-
-        // se o texto foi preenchido
-        if (strlen($texto)>=3) {
-            $sql = "SELECT codprodutobarra as id, codproduto, barras, descricao, sigla, preco, marca, referencia
-				      FROM vwProdutoBarra
-				     WHERE codProdutoBarra is not null ";
-
-            if (filter_var($request->inativo, FILTER_VALIDATE_BOOLEAN)) {
-                $sql .= "AND Inativo is null ";
+        $palavras = explode(' ', $request->busca);
+        $where = 'where';
+        $filtro = [];
+        foreach ($palavras as $i => $busca) {
+            $sql .= 'AND (';
+            // Verifica se foi digitado um valor e procura pelo preco
+            if ((numeroLimpo($busca) == $busca)
+                    && (strpos($busca, ",") != 0)
+                    && ((strlen($busca) - strpos($busca, ",")) == 3)) {
+                $sql .= "preco = :filtro{$i}";
+                $filtro["filtro{$i}"] = numeroLimpo($busca);
+            }
+            //senao procura por barras, descricao, marca e referencia
+            else {
+                $sql .= "barras ilike :filtro{$i} ";
+                $sql .= "OR descricao ilike :filtro{$i} ";
+                $sql .= "OR referencia ilike :filtro{$i} ";
+                $filtro["filtro{$i}"] = "%{$busca}%";
             }
 
-            $palavras = explode(' ', $request->busca);
-            $where = 'where';
-            $filtro = [];
-            foreach ($palavras as $i => $texto) {
-                $sql .= 'AND (';
-                // Verifica se foi digitado um valor e procura pelo preco
-                if ((numeroLimpo($texto) == $texto)
-                        && (strpos($texto, ",") != 0)
-                        && ((strlen($texto) - strpos($texto, ",")) == 3)) {
-                    $sql .= "preco = :filtro{$i}";
-                    $filtro["filtro{$i}"] = numeroLimpo($texto);
-                }
-                //senao procura por barras, descricao, marca e referencia
-                else {
-                    $sql .= "barras ilike :filtro{$i} ";
-                    $sql .= "OR descricao ilike :filtro{$i} ";
-                    $sql .= "OR referencia ilike :filtro{$i} ";
-                    $filtro["filtro{$i}"] = "%{$texto}%";
-                }
-
-                $sql .= ')';
-            }
-
-            //ordena
-            $sql .= " ORDER BY $ordem LIMIT $limite OFFSET $offset";
-
-            $resultados = DB::select($sql, $filtro);
-            // return $resultados;
-            // for ($i=0; $i<sizeof($resultados);$i++) {
-            //     $resultados[$i]["codproduto"] = Yii::app()->format->formataCodigo($resultados[$i]["codproduto"], 6);
-            //     $resultados[$i]["preco"] = Yii::app()->format->formatNumber($resultados[$i]["preco"]);
-            //     if (empty($resultados[$i]["referencia"])) {
-            //         $resultados[$i]["referencia"] = "-";
-            //     }
-            // }
+            $sql .= ')';
         }
+
+        //ordena
+        $sql .= " ORDER BY $ordem LIMIT $limite OFFSET $offset";
+
+        return $sql;
+
+        $resultados = DB::select($sql, $filtro);
+        // return $resultados;
+        // for ($i=0; $i<sizeof($resultados);$i++) {
+        //     $resultados[$i]["codproduto"] = Yii::app()->format->formataCodigo($resultados[$i]["codproduto"], 6);
+        //     $resultados[$i]["preco"] = Yii::app()->format->formatNumber($resultados[$i]["preco"]);
+        //     if (empty($resultados[$i]["referencia"])) {
+        //         $resultados[$i]["referencia"] = "-";
+        //     }
+        // }
 
         // transforma o array em JSON
-        return
-                array(
-                    'mais' => count($resultados)==$limite?true:false,
-                    'pagina' => (int) $pagina,
-                    'itens' => $resultados
-                );
+        return $resultados;
 
         $sql = "
             with prod as (
--- utils/SQLs/FTS - Teste.sql
+++ utils/SQLs/FTS - Teste.sql
@@ -4,19 +4,31 @@ from tblpessoa p
 where to_tsvector('portuguese', p.pessoa || p.fantasia) @@ phraseto_tsquery('portuguese', 'migliorini')
 and to_tsvector('portuguese', p.pessoa || p.fantasia) @@ phraseto_tsquery('portuguese', 'fabio')
 
+
+	
 with prod as (
 	select 
 		p.codproduto, 
-		p.produto || coalesce(' ' || pv.variacao, '') || coalesce(' ' || pbv.variacao, ''),
-		pv.variacao, 
-		pb.variacao, 
-		coalesce(p.referencia, '') || coalesce(pv.referencia, '')  || coalesce(pb.referencia, '') as referencia
+		pv.codprodutovariacao,
+		pb.codprodutobarra,
+		pb.barras,
+		p.produto || coalesce(' ' || pv.variacao, '') || coalesce(' ' || pb.variacao, '') || coalesce(' C/' || cast(pe.quantidade as bigint), '') as produto,
+		coalesce(p.referencia, '') || coalesce(pv.referencia, '')  || coalesce(pb.referencia, '') as referencia,
+		coalesce(pe.preco, p.preco * coalesce(pe.quantidade, 1)) as preco,
+		coalesce(pv.inativo, p.inativo) as inativo,
+		pv.descontinuado
 	from tblproduto p
 	inner join tblprodutovariacao pv on (pv.codproduto = p.codproduto)
 	inner join tblprodutobarra pb on (pb.codprodutovariacao  = pv.codprodutovariacao and pb.codproduto = p.codproduto)
 	left join tblprodutoembalagem pe on (pe.codprodutoembalagem = pb.codprodutoembalagem)
+	where pe.codprodutoembalagem is not null
 )
 select * 
 from prod p
-limit 100
+where p.produto || ' ' || p.referencia ilike '%copimax%'
+and p.produto || ' ' || p.referencia ilike '%papel%'
+and p.produto || ' ' || p.referencia ilike '%a4%'
+and p.inativo is null
+order by p.produto, p.referencia, p.preco, p.codprodutobarra
+
 
