b6ac4d0af4db31c3c62a9053047c119cfef31279
Fabio Migliorini <fabio.migliorini@gmail.com>
2021-01-26 18:25:02 -0400

    [UPD] SQLs

--- utils/SQLs/Vendas por Caixa.sql
+++ utils/SQLs/Vendas por Caixa.sql
@@ -1,20 +1,21 @@
 select
 	date_trunc('month', n.lancamento) as mes,
-	n.codfilial,
+	f.filial,
 	u.usuario,
-	--sum(n.valortotal) as valortotal,
+	--sum(npb.valortotal) as valortotal,
 	count(npb.codnegocioprodutobarra) as quantidade --*
 from tblnegocio n
 inner join tblnaturezaoperacao nat on (nat.codnaturezaoperacao = n.codnaturezaoperacao)
 inner join tblusuario u on (u.codusuario = n.codusuario)
 inner join tblnegocioprodutobarra npb on (npb.codnegocio = n.codnegocio)
+inner join tblfilial f ON (f.codfilial = n.codfilial)
 where n.codnegociostatus = 2
-and n.lancamento between '2020-10-01' and '2020-10-31 23:59:59'
+and n.lancamento between '2021-01-01' and '2021-01-31 23:59:59'
 and nat.venda = 1
-and n.codfilial = 103
+and n.codfilial = 104
 group by
 	date_trunc('month', n.lancamento),
-	n.codfilial,
+	f.filial,
 	u.usuario
 order by 4 desc
 --limit 50
