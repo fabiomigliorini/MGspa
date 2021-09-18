8e434f015a04fd9c644633950be9c2eb88f8ccb0
Fabio Migliorini <fabio.migliorini@gmail.com>
2021-09-18 12:26:03 -0400

    [ADD] Parser OFX

---
+++ laravel/app/Mg/Portador/PortadorResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Mg\Portador;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class PortadorResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $ret = parent::toArray($request);
+        unset($ret['bbdevappkey']);
+        unset($ret['bbclientid']);
+        unset($ret['bbclientsecret']);
+        unset($ret['bbtoken']);
+        unset($ret['bbtokenexpiracao']);
+        return $ret;
+    }
+}
