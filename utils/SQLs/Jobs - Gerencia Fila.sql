e1a5246248134d0b3ac086f4ab7b12808bb3009a
Fabio Migliorini <fabio.migliorini@gmail.com>
2021-12-04 08:47:19 -0400

    [UPD] SQLs

--- utils/SQLs/Jobs - Gerencia Fila.sql
+++ utils/SQLs/Jobs - Gerencia Fila.sql
@@ -1,9 +1,9 @@
 ﻿
 delete from tbljobsspa where tbljobsspa.id not in (select min(id) from tbljobsspa dup group by dup.payload);
 delete from tbljobs where tbljobs.id not in (select min(id) from tbljobs dup group by dup.payload);
-
 select 'lara', queue, count(*), min(id), max(id) from tbljobs group by queue union all
 select 'spa', queue, count(*), min(id), max(id) from tbljobsspa group by queue order by queue
+/*
 
 update tbljobsspa set queue = 'default' where id in (select j.id from tbljobsspa j where queue = 'parado' order by j.id limit 10)
 
@@ -17,8 +17,8 @@ update tbljobsspa set queue = 'default' where payload ilike '%NFePHPResolver%'
 
 select * from tbljobsspa
 
-/*
 select * from tbljobs
+
 delete from tbljobs where id = 6144778;
 ALTER SEQUENCE tbljobs_id_seq RESTART;
 
