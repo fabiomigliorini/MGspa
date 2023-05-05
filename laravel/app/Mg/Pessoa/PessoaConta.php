ae1f1d7d6ef0aa256807bea95ef3e6edd6ff8ad2
Fabio Migliorini <fabio@mgpapelaria.com.br>
2023-05-05 12:19:26 -0400

    [ADD] Models Pessoa-Conta/Email/Endereco/Telefone

---
+++ laravel/app/Mg/Pessoa/PessoaConta.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by php artisan gerador:model.
+ * Date: 25/Feb/2023 12:40:33
+ */
+
+namespace Mg\Pessoa;
+
+use Mg\MgModel;
+use Mg\Pessoa\Pessoa;
+use Mg\Usuario\Usuario;
+
+class PessoaConta extends MgModel
+{
+    protected $table = 'tblpessoaconta';
+    protected $primaryKey = 'codpessoaconta';
+
+
+    protected $fillable = [
+        'agencia',
+        'banco',
+        'cnpj',
+        'codpessoa',
+        'conta',
+        'inativo',
+        'pixaleatoria',
+        'pixcnpj',
+        'pixcpf',
+        'pixemail',
+        'pixtelefone',
+        'tipo'
+    ];
+
+    protected $dates = [
+        'alteracao',
+        'criacao',
+        'inativo'
+    ];
+
+    protected $casts = [
+        'banco' => 'float',
+        'cnpj' => 'float',
+        'codpessoa' => 'integer',
+        'codpessoaconta' => 'integer',
+        'codusuarioalteracao' => 'integer',
+        'codusuariocriacao' => 'integer',
+        'pixcnpj' => 'integer',
+        'pixcpf' => 'integer',
+        'pixtelefone' => 'float',
+        'tipo' => 'float'
+    ];
+
+
+    // Chaves Estrangeiras
+    public function Pessoa()
+    {
+        return $this->belongsTo(Pessoa::class, 'codpessoa', 'codpessoa');
+    }
+
+    public function UsuarioAlteracao()
+    {
+        return $this->belongsTo(Usuario::class, 'codusuarioalteracao', 'codusuario');
+    }
+
+    public function UsuarioCriacao()
+    {
+        return $this->belongsTo(Usuario::class, 'codusuariocriacao', 'codusuario');
+    }
+
+}
