49d03390ce410fa87f33ef2156a9eea5fe2d14c1
Fabio Migliorini <fabio@mgpapelaria.com.br>
2024-01-22 18:53:58 -0400

    [UPD] Ver

--- negocios/src/layouts/MainLayout.vue
+++ negocios/src/layouts/MainLayout.vue
@@ -28,7 +28,7 @@ const toggleRightDrawer = () => {
           </q-avatar>
           Negócios
         </q-toolbar-title>
-        <div class="gt-xs q-mr-sm">v0.3</div>
+        <div class="gt-xs q-mr-sm">v0.4</div>
 
         <usuario-conectado />
         &nbsp;
