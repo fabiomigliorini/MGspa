b6bc20c92c82ecfd6ae9dd7fb6ebfe5368f37aff
Fabio Migliorini <fabio@mgpapelaria.com.br>
2024-09-20 09:52:47 -0400

    [FIX] Qualidade Brilho/Contraste Confissao de Divida

--- laravel/app/Mg/Pdv/PdvAnexoService.php
+++ laravel/app/Mg/Pdv/PdvAnexoService.php
@@ -93,7 +93,7 @@ class PdvAnexoService
 
         // deixa em preto e branco
         imagefilter($anexo, IMG_FILTER_GRAYSCALE);
-        imagefilter($anexo, IMG_FILTER_BRIGHTNESS, 5);
+        imagefilter($anexo, IMG_FILTER_BRIGHTNESS, 0);
         imagefilter($anexo, IMG_FILTER_CONTRAST, -100);
 
         // redimensiona
