87e2318d7df35203c71643c043f566efc6e60a24
Wilhan Azevedo Dionizio <wilhan.azevedo@ambrait.com>
2024-11-23 15:47:52 -0400

    [FIX] ajuste troca de usuario

--- quasar/src/boot/axios.js
+++ quasar/src/boot/axios.js
@@ -9,6 +9,22 @@ export default ({ Vue }) => {
 
   Vue.prototype.$axios.interceptors.request.use(function (config) {
     const AUTH_TOKEN = document.cookie.split(';').find((item) => item.trim().startsWith('access_token='));
+    if(localStorage.getItem('auth.usuario.codusuario')){
+      const codusuario = localStorage.getItem('auth.usuario.codusuario')
+      const user_id_cookie = document.cookie.split(';').find((item) => item.trim().startsWith('user_id='));
+      if(user_id_cookie){
+        const user_id = user_id_cookie.split('=')[1]
+        if(codusuario !== user_id){
+          localStorage.removeItem('auth.token');
+          localStorage.removeItem('auth.usuario.usuario');
+          localStorage.removeItem('auth.usuario.codusuario');
+          localStorage.removeItem('auth.usuario.avatar');
+          let url = new URL(window.location.href)
+          url = encodeURI(url.origin)
+          window.location.href = process.env.API_AUTH_URL + '/login?redirect_uri=' + url
+        }
+      }
+    }
 
     if (AUTH_TOKEN) {
       let token = AUTH_TOKEN.split('=')[1]
