3a1ac60c26fc02ee06d4eae698f7f80a00820228
Fabio Migliorini <fabio@mgpapelaria.com.br>
2025-01-04 18:54:13 -0400

    [FIX] Icone/Pesquisa Impressoras

--- negocios/src/components/selects/SelectImpressora.vue
+++ negocios/src/components/selects/SelectImpressora.vue
@@ -20,33 +20,26 @@ const pesquisa = (val, update) => {
   update(() => {
     const pesquisa = val.toLowerCase();
     filtrado.value = opcoes.value.filter(
-      (item) => item.impressora.toLowerCase().indexOf(pesquisa) > -1
+      (item) => item.impressora.toLowerCase().indexOf(pesquisa) > -1 || item.nome.toLowerCase().indexOf(pesquisa) > -1
     );
   });
 };
 </script>
 <template>
-  <q-select
-    :options="filtrado"
-    use-input
-    @filter="pesquisa"
-    emit-value
-    map-options
-    option-value="impressora"
-    option-label="nome"
-    v-bind="$attrs"
-    options-cover
-  >
+  <q-select :options="filtrado" use-input @filter="pesquisa" emit-value map-options option-value="impressora"
+    option-label="nome" v-bind="$attrs" options-cover>
     <template v-slot:option="scope">
       <q-item v-bind="scope.itemProps">
-        <q-item-section side avatar>
-          <q-icon name="printer" />
+        <q-item-section avatar>
+          <q-icon name="print" />
         </q-item-section>
         <q-item-section>
-          <q-item-label class="ellipsis">{{ scope.opt.nome }}</q-item-label>
-          <q-item-label class="ellipsis" caption>{{
-            scope.opt.impressora
-          }}</q-item-label>
+          <q-item-label>
+            {{ scope.opt.nome }}
+          </q-item-label>
+          <q-item-label caption>
+            {{ scope.opt.impressora }}
+          </q-item-label>
         </q-item-section>
       </q-item>
     </template>
