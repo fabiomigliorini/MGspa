f44729458426aef7e32ae59fd0cf99273cda76a9
Fabio Migliorini <fabio@mgpapelaria.com.br>
2025-08-16 11:08:15 -0400

    [FIX] Quando expira a autenticacao, abre login

--- negocios/src/boot/axios.js
+++ negocios/src/boot/axios.js
@@ -3,20 +3,14 @@ import axios from "axios";
 import { usuarioStore } from "stores/usuario";
 const sUsuario = usuarioStore();
 
-// Be careful when using SSR for cross-request state pollution
-// due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
-// "export default () => {}" function below (which runs individually
-// for each client)
 const api = axios.create({ baseURL: process.env.API_BASE_URL });
 
 api.interceptors.request.use(
   (config) => {
     // Autorizacao
-    //get access_token from cookie
-
-    let tokenCookie = document.cookie.split(";").find((c) => c.trim().startsWith("access_token="));
+    let tokenCookie = document.cookie
+      .split(";")
+      .find((c) => c.trim().startsWith("access_token="));
     if (tokenCookie) {
       sUsuario.token.access_token = tokenCookie.split("=")[1];
     }
@@ -27,20 +21,39 @@ api.interceptors.request.use(
     return config;
   },
   (error) => {
-    Promise.reject(error);
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    // Retorna a resposta sem alteração se tudo estiver OK
+    return response;
+  },
+  (error) => {
+    // Verifica se a resposta do erro existe e se o status é 401 (nao autenticado)
+    if (error.response && error.response.status === 401) {
+      // Limpa o cookie de autenticação
+      document.cookie =
+        "access_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+      // Limpa o token e o usuario no store
+      sUsuario.usuario = {};
+      sUsuario.token = {};
+
+      // abre o dialog de login
+      sUsuario.dialog.login = true;
+
+      return Promise.reject(error);
+    }
+    // Para qualquer outro erro, o erro é propagado
+    return Promise.reject(error);
   }
 );
 
 export default boot(({ app }) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
-
   app.config.globalProperties.$axios = axios;
-  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
-  //       so you won't necessarily have to import axios in each vue file
-
   app.config.globalProperties.$api = api;
-  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
-  //       so you can easily perform requests against your app's API
 });
 
 export { api };
