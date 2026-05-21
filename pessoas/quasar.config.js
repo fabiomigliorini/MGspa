20d401298fc6dd1b991113c84781ebe25b7973f1
Fabio Migliorini <fabio.migliorini@gmail.com>
2026-05-21 17:59:25 -0400

    [FIX] Alias vue-router

--- pessoas/quasar.config.js
+++ pessoas/quasar.config.js
@@ -61,6 +61,7 @@ export default defineConfig((/* ctx */) => {
       alias: {
         '@components': path.resolve(import.meta.dirname, '../components'),
         'quasar/src': path.resolve(import.meta.dirname, 'node_modules/quasar/src'),
+        'vue-router': path.resolve(import.meta.dirname, 'node_modules/vue-router'),
       },
 
       vueRouterMode: 'history',
